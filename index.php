6043ca0f13108ff4fa75286ab30b32641105f843
Valeria415 <31986489+Valeria415@users.noreply.github.com>
2018-03-06 11:57:30 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,39 @@
+<html>
+<head>
+<Title>Ипотека</Title>
+<style type="text/css">
+    body { background-color:
+ #fff; border-top: solid 10px #000;
+ color: #333; font-size: .85em;
+ margin: 20; padding: 20;
+ font-family: "Segoe UI",
+ Verdana, Helvetica, Sans-Serif;
+    }
+    h1, h2, h3,{ color: #000;
+margin-bottom: 0; padding-bottom: 0; }
+    h1 { font-size: 2em; }
+    h2 { font-size: 1.75em; }
+    h3 { font-size: 1.2em; }
+    table { margin-top: 0.75em; }
+    th { font-size: 1.2em;
+ text-align: left; border: none; padding-left: 0; }
+    td { padding: 0.25em 2em 0.25em 0em;
+border: 0 none; }
+</style>
+</head>
+<body>
+<h1>Оформление ипотеки</h1>
+<p>Введите свои данные и перейдите по кнопке<strong> "Далее" </strong> .</p>
+<form method="post" action="index.php"
+      enctype="multipart/form-data" >
+Фамилия  <input type="text"
+       name="familiya" id="name"/></br>
+Имя  <input type="text"
+       name="name" id="password"/></br>  
+Отчество  <input type="text"
+       name="confirm_password" id="confirm_password"/></br>
+Email <input type="text"
+       name="Email" id="Email"/>
 
+</body>
+</html>
