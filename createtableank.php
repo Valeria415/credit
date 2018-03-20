c438e4298e547dfbaeb820b2dd1b4ceea75f23e0
Valeria415 <31986489+Valeria415@users.noreply.github.com>
2018-03-20 09:59:09 +0300

    Update and rename creat to createtableank.php

--- createtableank.php
+++ createtableank.php
@@ -2,10 +2,10 @@
 try {
     $conn = new PDO("sqlsrv:server = tcp:pinyasova.database.windows.net,1433; Database = Progr", "Valera", "Hswfhmlyz08");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "CREATE TABLE client_tbl(
+    $sql = "CREATE TABLE anketa_tbl(
     id INT NOT NULL IDENTITY(1,1) 
     PRIMARY KEY(id),
-    familiya VARCHAR(30),
+    pol VARCHAR(30),
     name VARCHAR(30),
     otchestvo VARCHAR(30),
     birthday DATE,
