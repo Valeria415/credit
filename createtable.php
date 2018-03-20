1db069e2eb037e5efcd8aee058442b5dfb285a8e
Valeria415 <31986489+Valeria415@users.noreply.github.com>
2018-03-20 11:46:55 +0300

    Update createtable.php

--- createtable.php
+++ createtable.php
@@ -16,7 +16,23 @@ try {
     nomerp VARCHAR(30),
     kem VARCHAR(30),
     data DATE,
-    kodp VARCHAR(30)
+    kodp VARCHAR(30),
+    pol INTEGER(2),
+    age INTEGER(3),
+    sp INTEGER(4),
+    ij VARCHAR(30),
+    educat INTEGER(3),
+    work INTEGER(3),
+    dolj INTEGER(2),
+    sel INTEGER(4),
+    prsel INTEGER(4),
+    credit INTEGER(3),
+    nepcredit INTEGER(2),
+    prop INTEGER(4),
+    hom VARCHAR(30),
+    srok INTEGER(4),
+    nachkap VARCHAR(30),
+    ugot INTEGER(4),
     )";
    
     $conn->query($sql);
